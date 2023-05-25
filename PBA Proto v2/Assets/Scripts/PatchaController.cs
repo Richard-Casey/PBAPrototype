4afb69d512453b6e675dcafe3c265f14546fb470
Richard Casey <91665484+Richard-Casey@users.noreply.github.com>
2023-05-25 20:37:25 +0100

    050523 20:37

--- PBA Proto v2/Assets/Scripts/PatchaController.cs
+++ PBA Proto v2/Assets/Scripts/PatchaController.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class PatchaController : MonoBehaviour
 {
-    //Patcha Variables
+    // Patcha Variables
     public int maxHealth = 100;
     public int currentHealth;
     public int Lives = 3;
     public int Pickups = 0;
     public int pickupCounter;
 
-    //movement variables
+    // Movement variables
     public float speed = 10f;
     public float jumpForce = 10f;
     public Vector3 movement;
 
-
-    //Component References
+    // Component References
     private Rigidbody Rbody;
+    public bool isGrounded; // Flag to track if the player is grounded
 
-    //Script References
+    // Script References
     public HealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        //initialise components
+        // Initialise components
         Rbody = GetComponent<Rigidbody>();
 
-        //initialise variables
+        // Initialise variables
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
@@ -37,47 +37,50 @@ public class PatchaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //damage test
+        // Damage test
         if (Input.GetKeyDown(KeyCode.K))
         {
             TakeDamage(20);
         }
-        //Gain life Test
+        // Gain life Test
         if (Input.GetKeyDown(KeyCode.L))
         {
             AddLife(1);
         }
-        //Lose Life Test
+        // Lose Life Test
         if (currentHealth <= 0)
         {
             LoseLife(1);
             currentHealth = maxHealth;
             healthBar.SetMaxHealth(maxHealth);
         }
-        //Pickup Test
+        // Pickup Test
         if (Input.GetKeyDown(KeyCode.P))
         {
             PickupFunc(1);
         }
-        //Life from pickup Test
+        // Life from pickup Test
         if (pickupCounter >= 100)
         {
             AddLife(1);
             pickupCounter = 0;
             Debug.Log("You have gained a life from Pickups");
         }
-        //movement code
+        // Movement code
         movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
+        // Jumping input
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
     }
+
     void FixedUpdate()
     {
         MoveChar(movement);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Rbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
     }
+
     // Custom Functions for Patcha
     void TakeDamage(int damage)
     {
@@ -85,13 +88,15 @@ public class PatchaController : MonoBehaviour
         healthBar.SetHealth(currentHealth);
         Debug.Log("Take Damage");
     }
+
     void AddLife(int Life)
     {
         Lives += Life;
-        //Life Adding Test
+        // Life Adding Test
         Debug.Log("Your Current Lives Are");
         Debug.Log(Lives);
     }
+
     void LoseLife(int lifelost)
     {
         Lives -= lifelost;
@@ -99,14 +104,38 @@ public class PatchaController : MonoBehaviour
         Debug.Log(Lives);
         Debug.Log("Remaining");
     }
+
     void PickupFunc(int PickupValue)
     {
         Pickups += PickupValue;
         pickupCounter += PickupValue;
         Debug.Log("You have picked up a coin");
     }
+
     void MoveChar(Vector3 direction)
     {
         Rbody.velocity = direction * speed;
-    }     
- }
+    }
+
+    void Jump()
+    {
+        Rbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    // Detect if the player is grounded
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
